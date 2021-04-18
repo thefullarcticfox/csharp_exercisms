0f452c2fc9832fd12fc242eb7876af928ba2b1c1
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-18 22:35:36 +0300

    +2 exercises

---
+++ pangram/Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+		input = input.ToLower();
+		for (char c = 'a'; c <= 'z'; c++)
+			if (!input.Contains(c))
+				return false;
+        return true;
+    }
+}
