c194c1b72d481a2bca7930d4c83b0d32cfa4d7f0
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-19 12:41:59 +0300

    +3 exercises

---
+++ acronym/Acronym.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string[] split = phrase.Split(" -.,".ToCharArray());
+
+        string res = "";
+        for (int i = 0; i < split.Length; i++)
+            for (int j = 0; j < split[i].Length; j++)
+                if (Char.IsLetter(split[i][j])) {
+                    res += split[i][j];
+                    break;
+                }
+        return res.ToUpper();
+    }
+}
---
+++ isogram/Isogram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        char[] arr = word.ToLower().ToCharArray();
+        Array.Sort(arr);
+        for (int i = 1; i < arr.Length; i++)
+            if (Char.IsLetter(arr[i]) && arr[i - 1] == arr[i])
+                return false;
+        return true;
+    }
+}
