b5ea689696ecc126371ac6e5b9cab0a02a05476b
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-19 13:26:36 +0300

    +3 exercises

---
+++ grains/Grains.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n) {
+        if (n < 1 || n > 64)
+            throw new ArgumentOutOfRangeException("Bad argument");
+        return 1UL << (n - 1);
+    }
+
+    public static ulong Total() => ~(new UInt64());
+}
---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number <= 0)
+            throw new ArgumentOutOfRangeException("Zero or not a natural number");
+        if (number == 1)
+            return Classification.Deficient;
+        int divsum = 1;
+        for (int i = 2; i < number; i++)
+            if (number % i == 0)
+                divsum += i;
+        if (divsum > number)
+            return Classification.Abundant;
+        if (divsum < number)
+            return Classification.Deficient;
+        return Classification.Perfect;
+    }
+}
