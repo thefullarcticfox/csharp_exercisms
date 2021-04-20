fe81ecd78b100779e296aebb5650c1fa48661cb5
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-20 23:55:07 +0300

    better spaceage

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -2,25 +2,25 @@ using System;
 
 public class SpaceAge
 {
-    private double _seconds;
-    public SpaceAge(int seconds)
-    {
-        _seconds = Convert.ToDouble(seconds);
-    }
+    private const double EARTH_YEAR_SECONDS = 31_557_600;
+    private readonly double _seconds;
+    private double Calculate(double orbitPeriod) =>
+        _seconds / EARTH_YEAR_SECONDS / orbitPeriod;
+    public SpaceAge(int seconds) => _seconds = (double)seconds;
 
-    public double OnEarth() => _seconds / 31557600;
+    public double OnEarth() => Calculate(1.0);
 
-    public double OnMercury() => OnEarth() / 0.2408467;
+    public double OnMercury() => Calculate(0.2408467);
 
-    public double OnVenus() => OnEarth() / 0.61519726;
+    public double OnVenus() => Calculate(0.61519726);
 
-    public double OnMars() => OnEarth() / 1.8808158;
+    public double OnMars() => Calculate(1.8808158);
 
-    public double OnJupiter() => OnEarth() / 11.862615;
+    public double OnJupiter() => Calculate(11.862615);
 
-    public double OnSaturn() => OnEarth() / 29.447498;
+    public double OnSaturn() => Calculate(29.447498);
 
-    public double OnUranus() => OnEarth() / 84.016846;
+    public double OnUranus() => Calculate(84.016846);
 
-    public double OnNeptune() => OnEarth() / 164.79132;
+    public double OnNeptune() => Calculate(164.79132);
 }
