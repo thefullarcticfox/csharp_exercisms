eea65da39fb72e70910e55ed1ef5d8c2736847c8
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-20 13:22:53 +0300

    +1 exercise

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        HashSet<int>    foundmultiples = new HashSet<int>();
+        foreach (int num in multiples)
+            for (int i = 1; num > 0 && num * i < max; i++)
+                foundmultiples.Add(num * i);
+        return foundmultiples.Sum();
+    }
+}
