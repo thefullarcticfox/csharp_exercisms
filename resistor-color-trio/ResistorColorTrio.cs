93bf571c549d6eb4fe84b8df73ca0f77802eaa32
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-20 14:34:06 +0300

    simpler resistors

--- resistor-color-trio/ResistorColorTrio.cs
+++ resistor-color-trio/ResistorColorTrio.cs
@@ -3,22 +3,18 @@ using System.Collections.Generic;
 
 public static class ResistorColorTrio
 {
-    private static readonly Dictionary<string, int> _resistors = new Dictionary<string, int>() {
-        { "black", 0 }, { "brown", 1 }, { "red", 2 }, { "orange", 3 },
-        { "yellow", 4 }, { "green", 5 }, { "blue", 6 }, { "violet", 7 },
-        { "grey", 8 }, { "white", 9 }
-    };
-
+    private static string[] _colors =
+        new string[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
     public static string Label(string[] colors)
     {
         int ohms = 0;
 
         for (int i = 0; i < 2 && i < colors.Length; i++) {
             ohms *= 10;
-            ohms += _resistors[colors[i]];
+            ohms += Array.IndexOf(_colors, colors[i]);
         }
 
-        for (int i = 0; i < _resistors[colors[2]]; i++)
+        for (int i = 0; i < Array.IndexOf(_colors, colors[2]); i++)
             ohms *= 10;
 
         string prefix = "";
--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -3,24 +3,8 @@ using System.Collections.Generic;
 
 public static class ResistorColor
 {
-    private static readonly Dictionary<string, int> _resistors = new Dictionary<string, int>() {
-        { "black", 0 }, { "brown", 1 }, { "red", 2 }, { "orange", 3 },
-        { "yellow", 4 }, { "green", 5 }, { "blue", 6 }, { "violet", 7 },
-        { "grey", 8 }, { "white", 9 }
-    };
-    public static int ColorCode(string color)
-    {
-        return _resistors[color];
-    }
+    public static int ColorCode(string color) => Array.IndexOf(Colors(), color);
 
-    public static string[] Colors()
-    {
-        string[] res = new string[_resistors.Count];
-
-        int i = 0;
-        foreach (var pair in _resistors)
-            res[i++] = pair.Key;
-
-        return res;
-    }
+    public static string[] Colors() =>
+        new string[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
 }
