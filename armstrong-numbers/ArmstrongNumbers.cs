c15833d731edd7da283b7aea2eaa89a74ee662ff
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-20 19:47:30 +0300

    +armstrong

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        int[] digits = Array.ConvertAll(number.ToString().ToCharArray(), x => (int)x - '0');
+
+        int sum = 0;
+        foreach (int digit in digits) {
+            int power = 1;
+            for (int i = 0; i < digits.Length; i++)
+                power *= digit;
+            sum += power;
+        }
+        return number == sum;
+    }
+}
