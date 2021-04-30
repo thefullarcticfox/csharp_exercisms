4ca7d362920bc635545ae596311ee1879863a102
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-30 23:10:25 +0300

    +fix

--- high-scores/HighScores.cs
+++ high-scores/HighScores.cs
@@ -1,10 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class HighScores
 {
-    private List<int>   _scoreList;
+    private readonly List<int>  _scoreList;
 
     public HighScores(List<int> list) => _scoreList = list;
 
