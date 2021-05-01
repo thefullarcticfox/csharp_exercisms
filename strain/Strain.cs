169fb7b86cd2cf4d12b51e3cfe63a9ba2305e846
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-01 13:25:40 +0300

    strain yield

--- strain/Strain.cs
+++ strain/Strain.cs
@@ -5,19 +5,15 @@ public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        var res = new List<T>();
         foreach(var elem in collection)
             if (predicate(elem))
-                res.Add(elem);
-        return res;
+                yield return elem;
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
-        var res = new List<T>();
         foreach(var elem in collection)
             if (!predicate(elem))
-                res.Add(elem);
-        return res;
+                yield return elem;
     }
 }
