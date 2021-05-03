31ac7227a3152d3b4ab41bef6e9f7472a4e0d5ad
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-03 13:09:22 +0300

    better nucleotide-count

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -5,10 +5,10 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        sequence.ToUpper();
+        sequence = sequence.ToUpper();
         var nucleotides = new Dictionary<char, int>{{'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}};
         foreach (char nuc in sequence) {
-            if (!nucleotides.TryGetValue(nuc, out int val))
+            if (!nucleotides.ContainsKey(nuc))
                 throw new ArgumentException($"{nuc} is an invalid nucleotide");
             ++nucleotides[nuc];
         }
