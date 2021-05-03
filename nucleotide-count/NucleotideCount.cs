d2fe3f0401c89859daf86c2e73d27f499f84160d
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-03 12:45:14 +0300

    better nucleotide-count

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,23 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        sequence.ToUpper();
-        if (sequence.Any(nuc => !("ACGT".Contains(nuc))))
-            throw new ArgumentException("invalid nucleotide");
-        return new Dictionary<char, int>{
-            {'A', sequence.Count(nuc => nuc == 'A')},
-            {'C', sequence.Count(nuc => nuc == 'C')},
-            {'G', sequence.Count(nuc => nuc == 'G')},
-            {'T', sequence.Count(nuc => nuc == 'T')}
-        };
-    }
-
-    /*  another option is using foreach and TryGet
         sequence.ToUpper();
         var nucleotides = new Dictionary<char, int>{{'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}};
         foreach (char nuc in sequence) {
@@ -26,5 +13,5 @@ public static class NucleotideCount
             ++nucleotides[nuc];
         }
         return nucleotides;
-    */
+    }
 }
